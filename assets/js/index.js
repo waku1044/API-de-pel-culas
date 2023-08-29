6ddc7f907df83986b8fdc291bbfb01f4f996a0d4
Walter Gimenez <75543829+waku1044@users.noreply.github.com>
2023-08-29 11:07:54 -0300

    se creo paginacion con mas peliculas

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,26 +1,32 @@
-const $section = document.querySelector('[data-section]');
-let api = 'https://api.themoviedb.org/3/movie/popular?api_key=da48657c7b1251891162e15cb4d8fc8f&language=es-ES';
+import cargarPeliculas from '../server/server.js';
+const $btn_siguiente = document.querySelector('[data-siguiente]');
+const $btn_anterior = document.querySelector('[data-anterior]');
+const $count = document.querySelector('[data-count]');
 
 
-const cargarPeliculas = (destino)=>{
-    return fetch(api)
-    .then(response => response.json())
-    .then(data => data.results.forEach(pelicula =>{
-        let article = document.createElement('article');
-        let template = `<div class="card" style="width: 18rem;">
-        <img src="https://image.tmdb.org/t/p/w500/${pelicula.poster_path}" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">${pelicula.title}</h5>
-          <p class="card-text">${pelicula.overview}</p>
-          <a href="#" class="btn btn-primary">Ver mas</a>
-        </div>
-        </div>`;
-        article.innerHTML += template;
-        destino.appendChild(article)
-        
-    }))
-};
+let count = 1;
+$count.innerText = count;
+const siguiente = ()=>{
+  if(count < 500){
+    count += 1;
+    $count.innerText = count;
+    return cargarPeliculas(count)
+  }else{
+    count == 1;
+}
+}
+const anterior = ()=>{
+  if(count > 1){
+    count -= 1;
+    $count.innerText = count;
+    return cargarPeliculas(count)
+  }else{
+    count == 1;
+}
+}
 
-cargarPeliculas($section)
 
+$btn_siguiente.addEventListener('click',siguiente)
+$btn_anterior.addEventListener('click',anterior)
+cargarPeliculas(count)
 
--- assets/server/server.js
+++ assets/server/server.js
@@ -1,18 +1,21 @@
-const cargarPeliculas = (destino)=>{
-    return fetch(api)
+
+const cargarPeliculas = async(count)=>{
+  let api = `https://api.themoviedb.org/3/movie/popular?api_key=da48657c7b1251891162e15cb4d8fc8f&language=es-ES&page=${count}`;
+  let template = '';
+    await fetch(api)
     .then(response => response.json())
     .then(data => data.results.forEach(pelicula =>{
-        let article = document.createElement('article');
-        let template = `<div class="card" style="width: 18rem;">
-        <img src="https://image.tmdb.org/t/p/w500/${pelicula.poster_path}" class="card-img-top" alt="...">
-        <div class="card-body">
+        let article = document.querySelector('#article');
+        template += `<div class="card" style="width: 18rem;height:40rem">
+        <img src="https://image.tmdb.org/t/p/w500/${pelicula.poster_path}" class="card-img-top" alt="${pelicula.title}">
+        <div class="card-body overflow-auto">
           <h5 class="card-title">${pelicula.title}</h5>
           <p class="card-text">${pelicula.overview}</p>
           <a href="#" class="btn btn-primary">Ver mas</a>
         </div>
         </div>`;
-        article.innerHTML += template;
-        destino.appendChild(article)
+        article.innerHTML = template;
+        
         
     }))
 }
