98d6de999b19b4fa3063d15b538bd0bf394aaff8
Walter Gimenez <75543829+waku1044@users.noreply.github.com>
2024-06-21 14:53:47 -0300

    se agrego eye parta password

--- assets/js/login.js
+++ assets/js/login.js
@@ -5,9 +5,20 @@ let $password = document.querySelector('#inputPassword');
 let usuarioRegistrado = localStorage.getItem('usuario');
 let passwordRegistrado = localStorage.getItem('password');
 let btn_entrar = document.querySelector('[data-entrar]');
+const eye = document.querySelector('.eye');
 
 $usuario.focus();
 
+function handleEye(){
+    if($password.type == 'password'){
+        $password.type = 'text';
+        eye.attributes.name.value ='eye-off-outline';
+    }else{
+        $password.type = 'password';
+        eye.attributes.name.value = 'eye-outline';
+    }
+}
+eye.addEventListener('click',handleEye);
 function validarRegistro(e){
     e.preventDefault();
     
